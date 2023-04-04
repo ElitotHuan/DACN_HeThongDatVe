02fee9f08a4a351d8dfa7073704d7522d17bf749
nthduc <nguyenthaiduc0212@gmail.com>
2023-04-04 13:49:38 +0700

    UserService security password

---
+++ DatVePhim/src/main/java/com/example/services/RoleServiceImpl.java
@@ -0,0 +1,42 @@
+package com.example.services;
+
+import com.example.models.Role;
+import com.example.repositories.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RoleServiceImpl implements RoleService{
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+
+    @Override
+    public List<Role> findAll() {
+        return roleRepository.findAll();
+    }
+
+    @Override
+    public Optional<Role> findById(Integer id) {
+        return roleRepository.findById(id);
+    }
+
+    @Override
+    public Role save(Role role) {
+        return roleRepository.save(role);
+    }
+
+    @Override
+    public void remove(Integer id) {
+        roleRepository.deleteById(id);
+    }
+
+    @Override
+    public Role findByName(String name) {
+        return roleRepository.findByName(name);
+    }
+}
