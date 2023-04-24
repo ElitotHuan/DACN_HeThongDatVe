b1731b8c8a7a38ef9b2475a0dc100eab6583df9a
Eliot Huan <66371006+ElitotHuan@users.noreply.github.com>
2023-04-24 13:44:27 +0700

    Revert "Merge branch 'main' into Huan"
    This reverts commit 9fa149b54fa79da2d6e526ee8ba92fa37ca2fbbd, reversing
    changes made to 132ae2337d9e2c4e130366fe61e265a31c5c661b.

--- DatVePhim/src/main/java/com/example/services/RoleService.java
+++
@@ -1,9 +0,0 @@
-package com.example.services;
-
-import com.example.models.Role;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface RoleService extends GeneralService<Role> {
-    Role findByName(String name);
-}
