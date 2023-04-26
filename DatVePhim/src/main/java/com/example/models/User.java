f29823750974987d6d3c1c2c89dc947f2eb27132
nthduc <nguyenthaiduc0212@gmail.com>
2023-04-26 21:07:18 +0700

    :hammer: model User (người dùng)

---
+++ DatVePhim/src/main/java/com/example/models/User.java
@@ -0,0 +1,25 @@
+package com.example.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "user")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "full_name")
+    private String fullName;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "username")
+    private String userName;
+}
