44caee77a5f20f1e3ba06223d4404bfa9782c022
LocNlu <18130128@st.hcmuaf.edu.vn>
2023-05-11 20:31:09 +0700

    update sql

--- DatVePhim/src/main/webapp/WEB-INF/jsp/client/menu.jsp
+++ DatVePhim/src/main/webapp/WEB-INF/jsp/client/menu.jsp
@@ -3,16 +3,11 @@
 <body>
 <div class="menu">
     <ul>
-        <li><a href="/Home"><i class="home"></i></a></li>
-        <li><a href="videos">
-            <div class="video"><i class="videos"></i><i class="videos1"></i></div>
+        <li><a href="/"><i class="home"></i></a></li>
+        <li><a href="ticket">
+        <div class="bk"><i class="booking"></i><i class="booking1"></i></div>
         </a></li>
-        <li><a href="reviews">
-            <div class="cat"><i class="watching"></i><i class="watching1"></i></div>
-        </a></li>
-        <%--            <li><a href="ticket">--%>
-        <%--                <div class="bk"><i class="booking"></i><i class="booking1"></i></div>--%>
-        <%--            </a></li>--%>
+
     </ul>
 </div>
 </body>
--- database/insert_schedule.sql
+++ database/insert_schedule.sql
@@ -1,22 +1,15 @@
 use ticketbooking;
 INSERT INTO schedule (id,price, start_date, start_time, branch_id, movie_id, room_id)
-    values( 1, 60000, '2023-04-08', '15:30:00',1,1,1);
+    values( 1, 60000, '2023-04-08', '15:30:00',1,2,1);
     INSERT INTO schedule (id,price, start_date, start_time, branch_id, movie_id, room_id)
-    values( 2, 65000, '2023-04-09', '16:30:00',1,2,1);
+    values( 2, 65000, '2023-04-09', '16:30:00',1,3,2);
     INSERT INTO schedule (id,price, start_date, start_time, branch_id, movie_id, room_id)
-    values( 3, 70000, '2023-04-10', '17:30:00',1,3,1);
+    values( 3, 70000, '2023-04-10', '17:30:00',1,4,3);
     INSERT INTO schedule (id,price, start_date, start_time, branch_id, movie_id, room_id)
-    values( 4, 80000, '2023-04-11', '18:30:00',1,4,1);
+    values( 4, 80000, '2023-04-11', '18:30:00',2,6,4);
     INSERT INTO schedule (id,price, start_date, start_time, branch_id, movie_id, room_id)
-    values( 5, 100000, '2023-04-12', '07:30:00',1,5,1);
+    values( 5, 100000, '2023-04-12', '07:30:00',2,7,5);
     INSERT INTO schedule (id,price, start_date, start_time, branch_id, movie_id, room_id)
-    values( 6, 110000, '2023-04-13', '10:30:00',1,6,1);
-    INSERT INTO schedule (id,price, start_date, start_time, branch_id, movie_id, room_id)
-    values( 7, 60000, '2023-04-14', '12:30:00',1,7,1);
-    INSERT INTO schedule (id,price, start_date, start_time, branch_id, movie_id, room_id)
-    values( 8, 65000, '2023-04-15', '15:00:00',1,8,1);
-    INSERT INTO schedule (id,price, start_date, start_time, branch_id, movie_id, room_id)
-    values( 9, 80000, '2023-04-08', '20:00:00',1,9,1);
-    INSERT INTO schedule (id,price, start_date, start_time, branch_id, movie_id, room_id)
-    values( 10, 80000, '2023-04-08', '15:00:00',1,10,1);
+    values( 6, 110000, '2023-04-13', '10:30:00',2,8,6);
+   
     
