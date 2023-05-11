1e395f8dc6f103643d10fbf3a27211d14d9e1ac3
LocNlu <18130128@st.hcmuaf.edu.vn>
2023-05-11 19:08:49 +0700

    fix

--- DatVePhim/src/main/java/com/example/repositories/MovieRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repositories;
-
-import com.example.models.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MovieRepository extends JpaRepository<Movie, Integer> {
-}
