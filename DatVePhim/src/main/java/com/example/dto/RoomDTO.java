7ae709c247b61673c50e66011ac7f293ed818225
Eliot Huan <66371006+ElitotHuan@users.noreply.github.com>
2023-05-15 17:32:14 +0700

    Thêm giao diện + xử lý chức năng quản lý Branch và Room

---
+++ DatVePhim/src/main/java/com/example/dto/RoomDTO.java
@@ -0,0 +1,15 @@
+package com.example.dto;
+
+import lombok.Data;
+
+@Data
+public class RoomDTO {
+    private int id;
+
+    private String name;
+    private int capacity;
+    private double totalArea;
+
+    private int branchId;
+
+}
