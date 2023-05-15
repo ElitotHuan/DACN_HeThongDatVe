2212a1c897df87aee6c56d43c2319a3c6f6daf1f
Eliot Huan <66371006+ElitotHuan@users.noreply.github.com>
2023-05-15 22:54:26 +0700

    Giao diện và xử lý chức năng quản lý Branch + Room

--- DatVePhim/src/main/java/com/example/services/RoomService.java
+++ DatVePhim/src/main/java/com/example/services/RoomService.java
@@ -1,6 +1,8 @@
 package com.example.services;
 
+import com.example.dto.RoomDTO;
 import com.example.models.Room;
+import com.example.repositories.BranchRepository;
 import com.example.repositories.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,7 +15,35 @@ public class RoomService {
     @Autowired
     private RoomRepository roomRepository;
 
+    @Autowired
+    private BranchRepository branchRepository;
+
     public List<Room> getAll() {
         return roomRepository.findAll();
+    }
+
+    public Boolean addRoom(RoomDTO roomDTO) {
+        Room newRoom = new Room();
+        newRoom.setName(roomDTO.getName());
+        newRoom.setCapacity(roomDTO.getCapacity());
+        newRoom.setTotalArea(roomDTO.getTotalArea());
+        newRoom.setBranch(branchRepository.getById(roomDTO.getBranchId()));
+        roomRepository.save(newRoom);
+        return true;
+    }
+
+    public Boolean updateRoom(int id, RoomDTO roomDTO) {
+        Room newRoom = roomRepository.getById(id);
+        newRoom.setName(roomDTO.getName());
+        newRoom.setCapacity(roomDTO.getCapacity());
+        newRoom.setTotalArea(roomDTO.getTotalArea());
+        newRoom.setBranch(branchRepository.getById(roomDTO.getBranchId()));
+        roomRepository.save(newRoom);
+        return true;
+    }
+
+    public Boolean deleteRoom(int id) {
+        roomRepository.deleteById(id);
+        return true;
     }
 }
